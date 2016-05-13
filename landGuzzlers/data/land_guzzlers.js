a3ed10b7fc3b083f7bca8b57f384d46887f3538f
Katherine Schinkel <kms6bn@virginia.edu>
2016-05-12 23:02:25 -0400

    add land guzzlers graphic

---
+++ landGuzzlers/data/land_guzzlers.js
@@ -0,0 +1,50 @@
+var guzzlers = [
+  {
+    "title": "LARGE DOG",
+    "footprint": 1.1,
+    "subtitle": "",
+    "subtitle2": "",
+    "subtitle3": "",
+    "color": "#A5610A"
+  },
+  {
+    "title": "MEDIUM SIZED DOG",
+    "footprint": 0.84,
+    "subtitle": "CONSUMPTION PER YEAR",
+    "subtitle2": "164kg of meat, 95kg or cereals",
+    "subtitle3": "43.3m2 of land per 1kg of chicken (more for beef and lamb), 13.4m2 of land per 1kg of cereals",
+    "color": "#C09455"
+  },
+  {
+    "title": "TOYOTA LAND CRUISER",
+    "footprint": 0.41,
+    "subtitle": "10,000km DRIVEN PER YEAR",
+    "subtitle2": "55.1 gigajoules (includes energy required to fuel and construct)",
+    "subtitle3": "",
+    "color":"#CC1F5E"
+  },
+  {
+    "title": "VOLKSWAGEN GOLF",
+    "footprint": 0.18,
+    "subtitle": "",
+    "subtitle2": "",
+    "subtitle3": "",
+    "color": "#A91748"
+  },
+  {
+    "title": "CAT",
+    "footprint": 0.15,
+    "subtitle": "",
+    "subtitle2": "",
+    "subtitle3": "",
+    "color": "#F6981C"
+  },
+  {
+    "title": "HAMSTER",
+    "footprint": 0.014,
+    "subtitle": "",
+    "subtitle2": "",
+    "subtitle3": "",
+    "color": "#221E1F"
+  }
+]
